d1a7d3cebc6dbde0a6489f0479b50be9d4d51bec
Nikko <atuan.nikko@gmail.com>
2026-01-22 23:29:04 +0800

    feat: JWT Token authentication

---
+++ auth-service/app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized', 401]);
+        }
+
+        return $this->createResponse($token);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'data' => new UserResource($user)
+        ]);
+    }
+
+    public function refresh()
+    {
+        return $this->createResponse(auth()->refresh());
+    }
+
+    public function user()
+    {
+        return response()->json(auth()->user());
+    }
+
+    protected function createResponse($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => new UserResource(auth()->user())
+        ]);
+    }
+}
---
+++ auth-service/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::get('user', [AuthController::class, 'user']);
+    });
+});
