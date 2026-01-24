bb47caafd0e1a514f0f585efac104a0b40f38596
Nikko <atuan.nikko@gmail.com>
2026-01-24 15:13:30 +0800

    feat: basic ip service setup

---
+++ ip-service/app/Models/IpAddress.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IpAddress extends Model
+{
+    protected $fillable = [
+        'ip_address',
+        'label',
+        'comment',
+        'created_by'
+    ];
+}
---
+++ ip-service/database/migrations/2026_01_24_071150_create_ip_addresses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ip_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip_address');
+            $table->string('label');
+            $table->text('comment')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ip_addresses');
+    }
+};
